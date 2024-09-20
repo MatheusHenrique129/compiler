530602b0f0814e8141df0ac39aad08daa0a639f0
Gustavo Goes <59123330+gustavogoess@users.noreply.github.com>
2024-09-19 23:03:17 -0300

    Classe sintatica

--- src/teste.py
+++ src/teste.py
@@ -71,6 +71,7 @@ class LexiconAnalyzer:
         self.line = 1
         self.buffer = buffer + '\0'
         self.i = 0
+        self.current_token = None
     
     def next_char(self):
         c = self.buffer[self.i]
@@ -233,180 +234,161 @@ class LexiconAnalyzer:
 
         return None  
 
-    def __init__(self, lexicon_analyzer):
-        self.lex = lexicon_analyzer
-        self.current_atom = self.lex.next_atom()
-
-    def next(self):
-        """Avança para o próximo átomo."""
-        self.current_atom = self.lex.next_atom()
-
-    def expect(self, atom_type, error_message):
-        """Verifica se o átomo atual é do tipo esperado e avança."""
-        if self.current_atom.type == atom_type:
-            self.next()
-        else:
-            self.error(error_message)
-
     def error(self, message):
-        """Levanta um erro de sintaxe."""
-        raise SyntaxError(f"Erro na linha {self.current_atom.line}: {message}")
+        raise Exception(f"Erro sintático na linha {self.current_token.line}: {message}")
 
-    # Funções de análise para cada regra da gramática
-
-    def analyze_program(self):
-        """<programa> ::= program identificador [( <lista de identificadores> )] ; <bloco>."""
-        self.expect(PROGRAM, "Esperado 'program' no início.")
-        self.expect(IDENTIFIER, "Esperado identificador após 'program'.")
-        if self.current_atom.type == PARENTESES:  # Se houver uma lista de identificadores entre parênteses
-            self.analyze_identifier_list()
-        self.expect(PONTO_VIRG, "Esperado ';' após a declaração do programa.")
-        self.analyze_block()
-
-    def analyze_block(self):
-        """<bloco> ::= [<declarações de variáveis>] <comando composto>."""
-        if self.current_atom.type == VAR:
-            self.analyze_variable_declarations()
-        self.analyze_compound_command()
-
-    def analyze_variable_declarations(self):
-        """<declarações de variáveis> ::= var <declaração> {; <declaração> };"""
-        self.expect(VAR, "Esperado 'var' para declaração de variáveis.")
-        self.analyze_declaration()
-        while self.current_atom.type == PONTO_VIRG:
-            self.next()
-            self.analyze_declaration()
-
-    def analyze_declaration(self):
-        """<declaração> ::= <lista de identificadores> : <tipo>."""
-        self.analyze_identifier_list()
-        self.expect(RELOP, "Esperado ':' após lista de identificadores.")
-        self.analyze_type()
-
-    def analyze_identifier_list(self):
-        """<lista de identificadores> ::= identificador { , identificador }"""
-        self.expect(IDENTIFIER, "Esperado identificador.")
-        while self.current_atom.type == VIRGULA:
-            self.next()
-            self.expect(IDENTIFIER, "Esperado identificador após ','.")
-    
-    def analyze_type(self):
-        """<tipo> ::= integer | boolean."""
-        if self.current_atom.type in [INTEGER, BOOLEAN]:
-            self.next()
+    def consume(self, expected_type):
+        if self.current_token.type == expected_type:
+            self.current_token = self.next_atom()
         else:
-            self.error("Esperado 'integer' ou 'boolean' para tipo de variável.")
+            self.error(f"Esperado {atomo_msg[expected_type]}, encontrado {atomo_msg[self.current_token.type]}")
 
-    def analyze_compound_command(self):
-        """<comando composto> ::= begin <comando> { ; <comando> } end."""
-        self.expect(BEGIN, "Esperado 'begin' no início do comando composto.")
-        self.analyze_command()
-        while self.current_atom.type == PONTO_VIRG:
-            self.next()
-            self.analyze_command()
-        self.expect(END, "Esperado 'end' no final do comando composto.")
+    def parse(self):
+        self.current_token = self.next_atom()
+        self.programa()
 
-    def analyze_command(self):
-        """<comando> ::= <atribuição> | <comando de entrada> | <comando de saída> | <comando if> | <comando while> | <comando composto>."""
-        if self.current_atom.type == IDENTIFIER:
-            self.analyze_assignment()
-        elif self.current_atom.type == READ:
-            self.analyze_input_command()
-        elif self.current_atom.type == WRITE:
-            self.analyze_output_command()
-        elif self.current_atom.type == IF:
-            self.analyze_if_command()
-        elif self.current_atom.type == WHILE:
-            self.analyze_while_command()
-        elif self.current_atom.type == BEGIN:
-            self.analyze_compound_command()
+    def programa(self):
+        self.consume(PROGRAM)
+        self.consume(IDENTIFIER)
+        if self.current_token.type == PARENTESES and self.current_token.lexeme == '(':
+            self.consume(PARENTESES)
+            self.lista_de_identificadores()
+            self.consume(PARENTESES)
+        self.consume(PONTO_VIRG)
+        self.bloco()
+        self.consume(PONTO)
+
+    def bloco(self):
+        if self.current_token.type == VAR:
+            self.declaracoes_de_variaveis()
+        self.comando_composto()
+
+    def declaracoes_de_variaveis(self):
+        self.consume(VAR)
+        self.declaracao()
+        self.consume(PONTO_VIRG)
+        while self.current_token.type == IDENTIFIER:
+            self.declaracao()
+            self.consume(PONTO_VIRG)
+
+    def declaracao(self):
+        self.lista_de_identificadores()
+        self.consume(RELOP)  # ':' é um RELOP
+        self.tipo()
+
+    def lista_de_identificadores(self):
+        self.consume(IDENTIFIER)
+        while self.current_token.type == VIRGULA:
+            self.consume(VIRGULA)
+            self.consume(IDENTIFIER)
+
+    def tipo(self):
+        if self.current_token.type == INTEGER:
+            self.consume(INTEGER)
+        elif self.current_token.type == BOOLEAN:
+            self.consume(BOOLEAN)
         else:
-            self.error("Comando desconhecido ou inesperado.")
+            self.error("Tipo inválido")
 
-    def analyze_assignment(self):
-        """<atribuição> ::= identificador := <expressao> ;"""
-        self.expect(IDENTIFIER, "Esperado identificador na atribuição.")
-        self.expect(RELOP, "Esperado ':=' após identificador.")
-        self.analyze_expression()
-        self.expect(PONTO_VIRG, "Esperado ';' no final da atribuição.")
+    def comando_composto(self):
+        self.consume(BEGIN)
+        self.comando()
+        while self.current_token.type == PONTO_VIRG:
+            self.consume(PONTO_VIRG)
+            self.comando()
+        self.consume(END)
 
-    def analyze_input_command(self):
-        """<comando de entrada> ::= read ( <lista de identificadores> )"""
-        self.expect(READ, "Esperado 'read'.")
-        self.expect(PARENTESES, "Esperado '(' após 'read'.")
-        self.analyze_identifier_list()
-        self.expect(PARENTESES, "Esperado ')' no comando de leitura.")
-
-    def analyze_output_command(self):
-        """<comando de saida> ::= write ( <expressao> { , <expressao> } )"""
-        self.expect(WRITE, "Esperado 'write'.")
-        self.expect(PARENTESES, "Esperado '(' após 'write'.")
-        self.analyze_expression()
-        while self.current_atom.type == VIRGULA:
-            self.next()
-            self.analyze_expression()
-        self.expect(PARENTESES, "Esperado ')' no comando de saída.")
-
-    def analyze_if_command(self):
-        """<comando if> ::= if <expressao> then <comando> [else <comando>]"""
-        self.expect(IF, "Esperado 'if'.")
-        self.analyze_expression()
-        self.expect(THEN, "Esperado 'then'.")
-        self.analyze_command()
-        if self.current_atom.type == ELSE:
-            self.next()
-            self.analyze_command()
-
-    def analyze_while_command(self):
-        """<comando while> ::= while <expressao> do <comando>"""
-        self.expect(WHILE, "Esperado 'while'.")
-        self.analyze_expression()
-        self.expect(DO, "Esperado 'do'.")
-        self.analyze_command()
-
-    def analyze_expression(self):
-        """<expressao> ::= <expressao simples> [<operador relacional> <expressao simples>]"""
-        self.analyze_simple_expression()
-        if self.current_atom.type == RELOP:
-            self.next()
-            self.analyze_simple_expression()
-
-    def analyze_simple_expression(self):
-        """<expressao simples> ::= [+ | −] <termo> { <operador de adição> <termo> }"""
-        if self.current_atom.type in [SUM, SUB]:
-            self.next()
-        self.analyze_term()
-        while self.current_atom.type == ADDOP:
-            self.next()
-            self.analyze_term()
-
-    def analyze_term(self):
-        """<termo> ::= <fator> { <operador de multiplicação> <fator> }"""
-        self.analyze_factor()
-        while self.current_atom.type == MULOP:
-            self.next()
-            self.analyze_factor()
-
-    def analyze_factor(self):
-        """<fator> ::= identificador | numero | ( <expressao> ) | true | false | not <fator>"""
-        if self.current_atom.type == IDENTIFIER:
-            self.next()
-        elif self.current_atom.type == NUM_INT:
-            self.next()
-        elif self.current_atom.type == PARENTESES and self.current_atom.lexeme == '(':
-            self.next()
-            self.analyze_expression()
-            self.expect(PARENTESES, "Esperado ')' após expressão.")
-        elif self.current_atom.type == TRUE or self.current_atom.type == FALSE:
-            self.next()
-        elif self.current_atom.type == NOT:
-            self.next()
-            self.analyze_factor()
+    def comando(self):
+        if self.current_token.type == IDENTIFIER:
+            self.atribuicao()
+        elif self.current_token.type == READ:
+            self.comando_de_entrada()
+        elif self.current_token.type == WRITE:
+            self.comando_de_saida()
+        elif self.current_token.type == IF:
+            self.comando_if()
+        elif self.current_token.type == WHILE:
+            self.comando_while()
+        elif self.current_token.type == BEGIN:
+            self.comando_composto()
         else:
-            self.error("Esperado identificador, número, true, false ou expressão entre parênteses.")
+            self.error("Comando inválido")
 
+    def atribuicao(self):
+        self.consume(IDENTIFIER)
+        self.consume(RELOP)  # ':=' é um RELOP
+        self.expressao()
 
+    def comando_if(self):
+        self.consume(IF)
+        self.expressao()
+        self.consume(THEN)
+        self.comando()
+        if self.current_token.type == ELSE:
+            self.consume(ELSE)
+            self.comando()
 
+    def comando_while(self):
+        self.consume(WHILE)
+        self.expressao()
+        self.consume(DO)
+        self.comando()
+
+    def comando_de_entrada(self):
+        self.consume(READ)
+        self.consume(PARENTESES)
+        self.lista_de_identificadores()
+        self.consume(PARENTESES)
+
+    def comando_de_saida(self):
+        self.consume(WRITE)
+        self.consume(PARENTESES)
+        self.expressao()
+        while self.current_token.type == VIRGULA:
+            self.consume(VIRGULA)
+            self.expressao()
+        self.consume(PARENTESES)
+
+    def expressao(self):
+        self.expressao_simples()
+        if self.current_token.type == RELOP:
+            self.consume(RELOP)
+            self.expressao_simples()
+
+    def expressao_simples(self):
+        if self.current_token.type in [SUM, SUB]:
+            self.consume(self.current_token.type)
+        self.termo()
+        while self.current_token.type in [SUM, SUB, ADDOP]:
+            self.consume(self.current_token.type)
+            self.termo()
+
+    def termo(self):
+        self.fator()
+        while self.current_token.type in [MULT, DIV, MOD]:
+            self.consume(self.current_token.type)
+            self.fator()
+
+    def fator(self):
+        if self.current_token.type == IDENTIFIER:
+            self.consume(IDENTIFIER)
+        elif self.current_token.type in [NUM_INT, NUM_REAL]:
+            self.consume(self.current_token.type)
+        elif self.current_token.type == PARENTESES and self.current_token.lexeme == '(':
+            self.consume(PARENTESES)
+            self.expressao()
+            self.consume(PARENTESES)
+        elif self.current_token.type == TRUE:
+            self.consume(TRUE)
+        elif self.current_token.type == FALSE:
+            self.consume(FALSE)
+        elif self.current_token.type == NOT:
+            self.consume(NOT)
+            self.fator()
+        else:
+            self.error("Fator inválido")
+
+     
 def read_file():
     if len(sys.argv) > 1:
         file_name = sys.argv[1]
@@ -433,20 +415,30 @@ def consume(atomo, lex):
     if atomo.type == ERROR:
         print(f'Linha: {atomo.line} - átomo: {atomo_msg[atomo.type]} Erro na linha {atomo.line}. Caractere inesperado "{lex.buffer[lex.i-1]}"')
     else:
-        print(f'Linha: {atomo.line} - átomo: {atomo_msg[atomo.type]}. {atomo.line} linhas analisadas, programa lexicamente correto.')
+        print(f'Linha: {atomo.line} - átomo: {atomo_msg[atomo.type]}. {atomo.line} linhas analisadas, programa sintaticamente correto.')
+
+# def main():
+#     buffer = read_file()
+#     lex = LexiconAnalyzer(buffer)
+#     atomo = lex.next_atom()
+
+#     consume(atomo, lex)
+
+# main()
 
 def main():
-    buffer = read_file()  # Função que lê o arquivo de entrada
+    buffer = read_file()
     lex = LexiconAnalyzer(buffer)
-    sintax_analyzer = SintaxAnalyzer(lex)
+    #parser = SyntaxAnalyzer(lex)
     atomo=lex.next_atom()
-
     consume(atomo, lex)
     try:
-        sintax_analyzer.analyze_program()  # Começa pela regra do programa
-        print("Programa sintaticamente correto.")
-    except SyntaxError as e:
-        print(e)
-   
+        lex.parse()
+        print("Análise sintática concluída com sucesso.")
+    except Exception as e:
+        print(f"Erro na análise sintática: {str(e)}")
 
 main()
+
+#LEXICO RODANDO, SINTATICO COM OUT OF RANGE. RODAR AMBOS AO MESMO TEMPO, CASO UM ENCONTRE ERRO
+#PARAR PRINT
