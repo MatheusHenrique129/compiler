8e63b8b6343de5504057cb98121edc7d7ba6104d
TiemiWatanabe <82404229+TiemiWatanabe@users.noreply.github.com>
2024-09-20 20:26:18 -0300

    Deletando outros arquivos

--- src/atomo.py
+++
@@ -1,9 +0,0 @@
-from typing import NamedTuple, Union
-
-class Atomo(NamedTuple):
-    type: int
-    lexeme: str
-    value: Union [int, float]
-    operator: int               # LE, NE, LT, GE, GT, EQ
-    line: int
- 
