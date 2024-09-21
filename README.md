b10ec93932d194d2366fdc54d1e356af76e41a98
Gustavo Goes <59123330+gustavogoess@users.noreply.github.com>
2024-09-21 17:07:41 -0300

    codigo-base (#2)
    * Update CompiladorPascalLite.py
    
    Co-authored-by: Lohnii <83559358+Lohnii@users.noreply.github.com>
    Co-authored-by: TiemiWatanabe <82404229+TiemiWatanabe@users.noreply.github.com>
    Co-authored-by: Matheus Henrique <matheus.mhsantos@mercadolivre.com>

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-# Compiler - AC01
+# Compiler - Lexical and Syntactic Analysis
 
-Description about the project...
+The objective of this repository is to implement a Compiler with the lexical and syntactic analysis phases for a language based on the Pascal language, called PascalLite. The Compiler for PascalLite restricts the Pascal language to have only integer and logical **(boolean)** types, conditional statements **(if)** and repetition **(while)**. We will not implement the declaration and calls of functions in this language, the exception is the input **(read)** and output **(write)** functions implemented in a modified form.
+
+When implementing the Compiler, the lexical analyzer must meet the needs of the syntactic analyzer. The interaction between the lexical analyzer and the syntactic analyzer will take place through the _**consume()**_ function (from the syntactic analyzer) which will make calls to the _**obtain_atomo()**_ function (from the lexical analyzer).
+
+<img align="right" width="100%" height="250px" alt="flow of the project" height="110px" src="./public/assets/preview.png"/>
+
+ &nbsp;
+
+If you want to know more about the specifications of this project, read the job [requirements doc](./docs/requirements-doc.pdf).
 
 ## Tech Stack
 
@@ -18,70 +26,82 @@ Add the following to gettin's started
 git clone https://github.com/MatheusHenrique129/compiler.git
 ```
 
-2. **Create feature/branch**:
+3. **Running the project**:
 
-```bash
-git checkout -b feature/NAME
+In the `src` folder, run the command:
+
+```py
+python3 compilerPascalLite.py
 ```
 
-## 🤝 Collaborators
+## 🤝 Collaborators ✨
 
-Special thank you for all people that contributed for this project.
+✨ Special thank you for all people that contributed for this project.
 
 <table>
-  <tr>
-    <td align="center">
-      <a href="#">
-        <img src="https://avatars.githubusercontent.com/u/83559358?v=4" width="100px;" alt="Bruna Tiemi Tarumoto Watanabe Profile Picture"/><br>
-        <sub>
-          <b>Bruna Tiemi</b>
-        </sub>
-      </a>
-    </td>
-    <td align="center">
-      <a href="#">
-        <img src="https://avatars.githubusercontent.com/u/59123330?v=4" width="100px;" alt="Gustavo Goes Sant`ana Profile Picture"/><br>
-        <sub>
-          <b>Gustavo Goes Sant`ana</b>
-        </sub>
-      </a>
-    </td>
-  </tr>
-  <td align="center">
-      <a href="#">
-        <img src="https://avatars.githubusercontent.com/u/112651586?v=4" width="100px;" alt="Kaiky Amorim dos Santos Profile Picture"/><br>
-        <sub>
-          <b>Kaiky Amorim</b>
-        </sub>
-      </a>
-    </td>
-    <td align="center">
-      <a href="#">
-        <img src="https://avatars.githubusercontent.com/u/67923259?s=400&u=4e6fa46b0f1d7daf7ca23fb8c9ab3034e70c0149&v=4" width="100px;" alt="Matheus Henrique Santos da Silva Profile Picture"/><br>
-        <sub>
-          <b>Matheus Henrique</b>
-        </sub>
-      </a>
-    </td>
+  <tbody>
+    <tr>
+      <td align="center">
+        <a href="https://github.com/Lohnii">
+          <img src="https://avatars.githubusercontent.com/u/83559358?v=4" width="100%" alt="Bruna Tiemi Tarumoto Watanabe Profile Picture"/><br>
+          <sub>
+            <b>Bruna Tiemi</b>
+          </sub>
+        </a>
+      </td>
+      <td align="center">
+        <a href="https://github.com/gustavogoess">
+          <img src="https://avatars.githubusercontent.com/u/59123330?v=4" width="100%"alt="Gustavo Goes Sant`ana Profile Picture"/><br>
+          <sub>
+            <b>Gustavo Goes Sant`ana</b>
+          </sub>
+        </a>
+      </td>
+      <td align="center">
+        <a href="https://github.com/kiq0509">
+          <img src="https://avatars.githubusercontent.com/u/112651586?v=4" width="100%" alt="Kaiky Amorim dos Santos Profile Picture"/><br>
+          <sub>
+            <b>Kaiky Amorim</b>
+          </sub>
+        </a>
+      </td>
+      <td align="center">
+        <a href="https://github.com/MatheusHenrique129">
+          <img src="https://avatars.githubusercontent.com/u/67923259?s=400&u=4e6fa46b0f1d7daf7ca23fb8c9ab3034e70c0149&v=4" width="100%" alt="Matheus Henrique Santos da Silva Profile Picture"/><br>
+          <sub>
+            <b>Matheus Henrique</b>
+          </sub>
+        </a>
+      </td>
+    </tr>
+  </tbody>
 </table>
 
 ## 📫 Contribute
 
 Here you will explain how other developers can contribute to your project. For example, explaining how can create their branches, which patterns to follow and how to open an pull request
 
-1. `git clone https://github.com/Fernanda-Kipper/text-editor.git`
-2. `git checkout -b feature/NAME`
-3. Follow commit patterns
-4. Open a Pull Request explaining the problem solved or feature made, if exists, append screenshot of visual modifications and wait for the review!
+1. Clone project
+
+```bash
+  git clone https://github.com/MatheusHenrique129/compiler.git
+```
+
+2. Create feature/branch:
+
+```bash
+  git checkout -b feature/NAME
+```
+
+3. Follow [commit patterns](https://gist.github.com/joshbuchea/6f47e86d2510bce28f8e7f42ae84c716).
+
+4. Open a [Pull Request](https://www.atlassian.com/br/git/tutorials/making-a-pull-request) explaining the problem solved or feature made, if exists, append screenshot of visual modifications and wait for the review!
 
 ### Documentations that might help
 
-[📝 How to create a Pull Request](https://www.atlassian.com/br/git/tutorials/making-a-pull-request)
+- [📝 How to create a Pull Request](https://www.atlassian.com/br/git/tutorials/making-a-pull-request)
+- [💾 Commit pattern](https://gist.github.com/joshbuchea/6f47e86d2510bce28f8e7f42ae84c716)
 
-[💾 Commit pattern](https://gist.github.com/joshbuchea/6f47e86d2510bce28f8e7f42ae84c716)
+### License
 
-## License
-
-This software is available under the following licenses:
-
-- [MIT](https://rem.mit-license.org)
+This software is available under the following licenses: [MIT](https://rem.mit-license.org)
---
+++ src/teste.py
@@ -0,0 +1,445 @@
+from typing import NamedTuple
+from typing import Union
+import sys
+
+ERROR = 0
+IDENTIFIER = 1
+NUM_INT = 2
+NUM_REAL = 3
+EOS = 4
+RELOP = 5
+ADDOP = 6
+MULOP = 7
+
+# palavras reservadas
+IF = 8
+THEN = 9
+ELSE = 10
+BEGIN = 11
+END = 12
+BOOLEAN = 13
+DIV= 14
+DO= 15
+FALSE= 16
+INTEGER= 17
+MOD= 18
+PROGRAM= 19
+READ= 20
+TRUE= 21
+NOT= 22
+VAR= 23
+WHILE= 24
+WRITE= 25
+COMMENT = 26
+PONTO_VIRG = 27
+VIRGULA = 28
+PARENTESES = 29
+SUM = 30
+SUB = 31
+MULT = 32
+PONTO = 33
+
+
+# operador relacional
+LE = 1000
+NE = 1001
+LT = 1002
+GE = 1003
+GT = 1004
+EQ = 1005
+
+atomo_msg = ['Erro Sintático!', 'IDENTIFIER', 'NUM_INT   ', 'NUM_REAL', 'EOS',
+             'RELOP', 'ADDOP', 'MULOP', 'IF', 'THEN', 'ELSE', 'BEGIN' , 'END',
+             'BOOLEAN' ,'DIV','DO','FALSE','INTEGER','MOD','PROGRAM','READ',
+             'TRUE','NOT','VAR','WHILE','WRITE', 'COMMENT', 'PONTO_VIRG', 'VIRGULA', 'PARENTESES', 'SUM', 'SUB', 'MULT', 'PONTO']
+
+reserved_words = {'if': IF, 'then': THEN, 'else': ELSE, 'begin': BEGIN, 'end': END,
+                  'boolean': BOOLEAN, 'div': DIV, 'do': DO, 'false': FALSE, 'integer': INTEGER, 
+                  'mod': MOD, 'program': PROGRAM, 'read': READ, 'true': TRUE, 'not': NOT, 'var': VAR, 
+                  'while': WHILE, 'write': WRITE, 'comment':COMMENT, 'ponto_virg':PONTO_VIRG, 
+                  'virgula':VIRGULA, 'parenteses': PARENTESES, 'sum': SUM, 'sub': 'SUB', 'mult': MULT, 'ponto': PONTO}
+
+class Atomo(NamedTuple):
+    type: int
+    lexeme: str
+    value: Union [int, float]
+    operator: int               # LE, NE, LT, GE, GT, EQ
+    line: int
+
+class LexiconAnalyzer:
+    def __init__(self, buffer: str):
+        self.line = 1
+        self.buffer = buffer + '\0'
+        self.i = 0
+        self.current_token = None
+    
+    def next_char(self):
+        c = self.buffer[self.i]
+        self.i += 1
+        return c
+    
+    def retract(self):
+        self.i -= 1
+
+    # analisador lexico
+    def next_atom(self):
+        atomo = Atomo(ERROR, '', 0, 0, self.line)
+        c = self.next_char()
+        while (c in [' ', '\n', '\t', '\r', '\0']):
+            if (c == '\n'):
+                self.line += 1
+            if (c == '\0'):
+                return Atomo(EOS, '', 0, 0, self.line)
+            c = self.next_char()
+            
+        if c in ['/', '(', '{']:  
+            comentario = self.treat_comment(c)
+            if comentario:
+                return comentario
+        if c.isalpha() or c == '_':
+            return self.treat_identifier(c)
+        elif c.isdigit():
+            return self.treat_number(c)
+        elif c== ':':
+            nextChar=self.next_char()
+            if nextChar == '=':
+                return Atomo(RELOP,':=', 0, EQ, self.line)
+            else:
+                self.retract()
+                return Atomo(RELOP,':',0, 0, self.line)
+        elif c == '<' or c == '>':
+            return self.treat_operator_minor(c)
+        elif c == ':':
+            return Atomo(RELOP, ':', 0, EQ, self.line)
+        elif c == ';':
+            return Atomo(PONTO_VIRG, ';', 0, 0, self.line)
+        elif c == ',':
+            return Atomo(VIRGULA, ',', 0, 0, self.line)
+        elif c == '(':
+            return Atomo(PARENTESES, '(', 0, 0, self.line)
+        elif c == ')':
+            return Atomo(PARENTESES, ')', 0, 0, self.line)
+        elif c == '+':
+            return Atomo(SUM, '+', 0, 0, self.line)
+        elif c == '*':
+            return Atomo(MULT, '*', 0, 0, self.line)
+        elif c == '/':
+            return Atomo(DIV, '/', 0, 0, self.line)
+        elif c == '-':
+            return Atomo(SUB, '-', 0, 0, self.line)
+        elif c == '.':
+            return Atomo(PONTO, '.', 0, 0, self.line)
+        return atomo
+
+    def treat_operator_minor(self, c: str):
+        c = self.next_char()
+        state = 1
+        while True:
+            if state == 1:
+                if c == '=':
+                    state = 2
+                elif c == '>':
+                    state = 3
+                else:
+                    state = 4
+            elif state == 2:
+                return Atomo(RELOP, '<=', 0, LE, self.line)
+            elif state == 3:
+                return Atomo(RELOP, '<>', 0, NE, self.line)
+            elif state == 4:
+                self.retract()
+                return Atomo(RELOP, '<', 0, LT, self.line)
+
+    def treat_number(self, c: str):
+        lexeme = c
+        c = self.next_char()
+        state = 1
+        while True:
+            if state == 1:
+                if c.isdigit():
+                    lexeme += c
+                    state = 1
+                    c = self.next_char()
+                else:
+                    state = 2
+            elif state == 2:
+                self.retract()
+                return Atomo(NUM_INT, lexeme, int(lexeme), 0, self.line)
+
+    def treat_identifier(self, c: str):
+        lexeme = c
+        c = self.next_char()
+        state = 1
+        while True:
+            if state == 1:
+                if c.isdigit() or c.isalpha() or c == '_':
+                    lexeme += c
+                    if len(lexeme) > 20:
+                        return Atomo(ERROR, lexeme, 0, 0, self.line)
+                    state = 1
+                    c = self.next_char()
+                else:
+                    state = 2
+            elif state == 2:
+                self.retract()
+                if lexeme.lower() in reserved_words:
+                    reserved = reserved_words[lexeme.lower()]
+                    return Atomo(reserved, lexeme, 0, 0, self.line)
+                else:
+                    return Atomo(IDENTIFIER, lexeme, 0, 0, self.line)
+                
+    def treat_comment(self, initial: str):
+        lexeme = initial
+        if initial == '/':  
+            nextChar = self.next_char()
+            if nextChar == '/':
+                lexeme += nextChar
+                c = self.next_char()
+                while c != '\n' and c != '\0':  
+                    lexeme += c
+                    c = self.next_char()
+                self.line += 1
+                return Atomo(ERROR, lexeme, 0, 0, self.line)  
+            else:
+                self.retract()  
+                return None  
+
+        elif initial == '(': 
+            nextChar = self.next_char()
+            if nextChar == '*':
+                lexeme += nextChar
+                c = self.next_char()
+                while True:
+                    if c == '*' and self.next_char() == ')':
+                        lexeme += '*)'
+                        break
+                    lexeme += c
+                    if c == '\n':
+                        self.line += 1  
+                    c = self.next_char()
+                return Atomo(COMMENT, lexeme, 0, 0, self.line)  
+            else:
+                self.retract()  
+                return None  
+
+        elif initial == '{':  
+            c = self.next_char()
+            while c != '}' and c != '\0':  
+                lexeme += c
+                if c == '\n':
+                    self.line += 1  
+                c = self.next_char()
+            lexeme += '}' 
+            return Atomo(ERROR, lexeme, 0, 0, self.line) 
+
+        return None  
+
+    def error(self, message):
+        raise Exception(f"Erro sintático na linha {self.current_token.line}: {message}")
+
+    def consume(self, expected_type):
+        if self.current_token.type == expected_type:
+            self.current_token = self.next_atom()
+        else:
+            self.error(f"Esperado {atomo_msg[expected_type]}, encontrado {atomo_msg[self.current_token.type]}")
+
+    def parse(self):
+        self.current_token = self.next_atom()
+        self.programa()
+
+    def programa(self):
+        self.consume(PROGRAM)
+        self.consume(IDENTIFIER)
+        if self.current_token.type == PARENTESES and self.current_token.lexeme == '(':
+            self.consume(PARENTESES)
+            self.lista_de_identificadores()
+            self.consume(PARENTESES)
+        self.consume(PONTO_VIRG)
+        self.bloco()
+        self.consume(PONTO)
+
+    def bloco(self):
+        if self.current_token.type == VAR:
+            self.declaracoes_de_variaveis()
+        self.comando_composto()
+
+    def declaracoes_de_variaveis(self):
+        self.consume(VAR)
+        self.declaracao()
+        self.consume(PONTO_VIRG)
+        while self.current_token.type == IDENTIFIER:
+            self.declaracao()
+            self.consume(PONTO_VIRG)
+
+    def declaracao(self):
+        self.lista_de_identificadores()
+        self.consume(RELOP)  # ':' é um RELOP
+        self.tipo()
+
+    def lista_de_identificadores(self):
+        self.consume(IDENTIFIER)
+        while self.current_token.type == VIRGULA:
+            self.consume(VIRGULA)
+            self.consume(IDENTIFIER)
+
+    def tipo(self):
+        if self.current_token.type == INTEGER:
+            self.consume(INTEGER)
+        elif self.current_token.type == BOOLEAN:
+            self.consume(BOOLEAN)
+        else:
+            self.error("Tipo inválido")
+
+    def comando_composto(self):
+        self.consume(BEGIN)
+        self.comando()
+        while self.current_token.type == PONTO_VIRG:
+            self.consume(PONTO_VIRG)
+            self.comando()
+        self.consume(END)
+
+    def comando(self):
+        if self.current_token.type == IDENTIFIER:
+            self.atribuicao()
+        elif self.current_token.type == READ:
+            self.comando_de_entrada()
+        elif self.current_token.type == WRITE:
+            self.comando_de_saida()
+        elif self.current_token.type == IF:
+            self.comando_if()
+        elif self.current_token.type == WHILE:
+            self.comando_while()
+        elif self.current_token.type == BEGIN:
+            self.comando_composto()
+        else:
+            self.error("Comando inválido")
+
+    def atribuicao(self):
+        self.consume(IDENTIFIER)
+        self.consume(RELOP)  # ':=' é um RELOP
+        self.expressao()
+
+    def comando_if(self):
+        self.consume(IF)
+        self.expressao()
+        self.consume(THEN)
+        self.comando()
+        if self.current_token.type == ELSE:
+            self.consume(ELSE)
+            self.comando()
+
+    def comando_while(self):
+        self.consume(WHILE)
+        self.expressao()
+        self.consume(DO)
+        self.comando()
+
+    def comando_de_entrada(self):
+        self.consume(READ)
+        self.consume(PARENTESES)
+        self.lista_de_identificadores()
+        self.consume(PARENTESES)
+
+    def comando_de_saida(self):
+        self.consume(WRITE)
+        self.consume(PARENTESES)
+        self.expressao()
+        while self.current_token.type == VIRGULA:
+            self.consume(VIRGULA)
+            self.expressao()
+        self.consume(PARENTESES)
+
+    def expressao(self):
+        self.expressao_simples()
+        if self.current_token.type == RELOP:
+            self.consume(RELOP)
+            self.expressao_simples()
+
+    def expressao_simples(self):
+        if self.current_token.type in [SUM, SUB]:
+            self.consume(self.current_token.type)
+        self.termo()
+        while self.current_token.type in [SUM, SUB, ADDOP]:
+            self.consume(self.current_token.type)
+            self.termo()
+
+    def termo(self):
+        self.fator()
+        while self.current_token.type in [MULT, DIV, MOD]:
+            self.consume(self.current_token.type)
+            self.fator()
+
+    def fator(self):
+        if self.current_token.type == IDENTIFIER:
+            self.consume(IDENTIFIER)
+        elif self.current_token.type in [NUM_INT, NUM_REAL]:
+            self.consume(self.current_token.type)
+        elif self.current_token.type == PARENTESES and self.current_token.lexeme == '(':
+            self.consume(PARENTESES)
+            self.expressao()
+            self.consume(PARENTESES)
+        elif self.current_token.type == TRUE:
+            self.consume(TRUE)
+        elif self.current_token.type == FALSE:
+            self.consume(FALSE)
+        elif self.current_token.type == NOT:
+            self.consume(NOT)
+            self.fator()
+        else:
+            self.error("Fator inválido")
+
+     
+def read_file():
+    if len(sys.argv) > 1:
+        file_name = sys.argv[1]
+    else:
+        file_name = r'C:\Users\gugsr\OneDrive\Documents\GitHub\compiler\src\input02.pas'
+
+    arq = open(file_name)
+    buffer = arq.read()
+    arq.close()
+
+    return buffer
+
+def consume(atomo, lex):
+    while atomo.type not in [EOS, ERROR]:
+        print(f'Linha: {atomo.line} - átomo: {atomo_msg[atomo.type]}\t\t lexema: {atomo.lexeme}', end='')
+
+        if atomo.value != 0:
+            print(f'\t\t valor: {atomo.value}')
+        else:
+            print()
+            
+        atomo = lex.next_atom()
+        
+    if atomo.type == ERROR:
+        print(f'Linha: {atomo.line} - átomo: {atomo_msg[atomo.type]} Erro na linha {atomo.line}. Caractere inesperado "{lex.buffer[lex.i-1]}"')
+    else:
+        print(f'Linha: {atomo.line} - átomo: {atomo_msg[atomo.type]}. {atomo.line} linhas analisadas, programa sintaticamente correto.')
+
+# def main():
+#     buffer = read_file()
+#     lex = LexiconAnalyzer(buffer)
+#     atomo = lex.next_atom()
+
+#     consume(atomo, lex)
+
+# main()
+
+def main():
+    buffer = read_file()
+    lex = LexiconAnalyzer(buffer)
+    #parser = SyntaxAnalyzer(lex)
+    atomo=lex.next_atom()
+    consume(atomo, lex)
+    try:
+        lex = LexiconAnalyzer(buffer)
+        lex.parse()
+        print("Análise sintática concluída com sucesso.")
+    except Exception as e:
+        print(f"Erro na análise sintática: {str(e)}")
+
+main()
+
+#LEXICO RODANDO, SINTATICO COM OUT OF RANGE. RODAR AMBOS AO MESMO TEMPO, CASO UM ENCONTRE ERRO
+#PARAR PRINT
